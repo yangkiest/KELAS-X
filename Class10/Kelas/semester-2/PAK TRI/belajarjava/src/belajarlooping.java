601e609dd05e4897271fe1a04ee3231939b02f5c
yangkiest <yangkiest213@gmail.com>
2026-02-18 17:04:24 +0700

    add PAK TRI

--- Class10/Kelas/semester-2/PAK TRI/belajarjava/src/belajarlooping.java
+++ Class10/Kelas/semester-2/PAK TRI/belajarjava/src/belajarlooping.java
@@ -3,41 +3,57 @@ import java.util.Scanner;
 
     public static void main(String[] args) {
         
-        Scanner inputUser = new Scanner(System.in);
-        
-    System.out.print("masukkan nama anda: ");
-    String nama = inputUser.nextLine();
-        
-    System.out.print("masukkan jumlah perulangan: ");
-    int jumlah =  inputUser.nextInt();
-    
-    for (int i = 1; i <= jumlah; i++) {
-        System.out.println(i + ". " + nama);
-    }
-    
-    //segitiga siku siku 
-    System.out.print("masukkan tinggi segitiga: ");
-    int tinggi1 = inputUser.nextInt();
-    
-    for (int i = 1; i <= tinggi1; i++){
-        for (int j = 1; j <= i; j++){
-            System.out.print("*");
+         System.out.println("masukkan nomer urut: ");
+        Scanner input1 = new Scanner(System.in);
+        int angka = input1.nextInt();
+
+
+        System.out.println("masukkan kalimat atau kata: ");
+        Scanner nama = new Scanner(System.in);
+        String kata = nama.nextLine();
+        for (int i = 1; i <= angka; i++) {
+            System.out.println(i + ". " + kata);
         }
-        System.out.println();
+
+
+        Scanner tinggi = new Scanner(System.in);
+        System.out.println("masukkan tinggi segitiga: ");
+        int n = tinggi.nextInt();
+
+
+        /* TUGAS SEGITIGA*/
+        
+        // 1. Segitiga sudut bawah kiri
+        System.out.println("\nSegitiga Sudut Bawah Kiri");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 2. Segitiga sudut bawah kanan
+        System.out.println("\nSegitiga Sudut Bawah Kanan");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 3. Segitiga sudut atas kiri
+        System.out.println("\nSegitiga Sudut Atas Kiri");
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+       
     }
-    
-    //segitiga siku-siku terbalik
-    System.out.print("masukkan tinggi segitiga: ");
-    int tinggi2 = inputUser.nextInt();
-    
-    for (int i = tinggi2; i >= 1; i--){
-        for (int j = 1; j <= i; j++){
-            System.out.print("*");
-    }
-        System.out.println();
-    }
-    inputUser.close();
-    }
-    
 }
     
